26a4ceb7fb3a74dc0e9b82b875d06e99dd9dda40
OstapHetman <prickly4528@gmail.com>
2018-04-25 12:21:00 +0300

    add modals and dynamicall title text for it

--- src/js/main.js
+++ src/js/main.js
@@ -3,22 +3,45 @@ const servicesItems = document.querySelectorAll('.service__item__col:nth-child(n
 const viewMoreBtn = document.querySelector('.view-more');
 const viewMoreBtnG = document.querySelector('.view-more-g');
 const servicesItemsGeneral = document.querySelectorAll('.service__item-clients-col:nth-child(n+4)');
+const modalHeadText = document.querySelector('#head-text');
+const hiddenInput = document.querySelector('#hidden-input');
 
+// Change text on mouseenter
 for (var i = 0; i < className.length; i++) {
     className[i].addEventListener('mouseenter', (e) => {
         e.target.innerHTML = 'Оставить заявку';
+        e.preventDefault();
+    }, false);
+    className[i].addEventListener('click', (e) => {
+        e.preventDefault();
     }, false);
 }
 
+// Change text on mouseleave
 for (var i = 0; i < className.length; i++) {
     className[i].addEventListener('mouseleave', (e) => {
         e.target.innerHTML = 'Узнать подробнее';
+        e.preventDefault();
+    }, false);
+    className[i].addEventListener('click', (e) => {
+        e.preventDefault();
+    }, false);
+}
+
+// Add text on modal
+for (var i = 0; i < className.length; i++) {
+    className[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        let titleText = e.target.getAttribute('data-title');
+        modalHeadText.innerText = titleText;
+        hiddenInput.value = titleText;
     }, false);
 }
 
 viewMoreBtn.addEventListener('click', hideCols);
 viewMoreBtnG.addEventListener('click', hideColsG);
 
+// Show more block
 function hideCols(e) {
     e.preventDefault();
     for (let item of servicesItems) {
@@ -28,7 +51,7 @@ function hideCols(e) {
         }
     }
 }
-
+// Show more block
 function hideColsG(e) {
     e.preventDefault();
     for (let item of servicesItemsGeneral) {
@@ -38,6 +61,33 @@ function hideColsG(e) {
         }
     }
 }
+
+// JQUERY
+
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $(".smooth").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
 
 // Scroll Top
 $(window).scroll(function() {
